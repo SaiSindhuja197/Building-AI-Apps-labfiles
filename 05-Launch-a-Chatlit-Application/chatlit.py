e7a313d24f167b225e5d60b296a08b6cf4d966cc
shashankms9 <104433795+shashankms9@users.noreply.github.com>
2024-10-01 21:03:12 +0530

    Update chatlit.py

--- 05-Launch-a-Chatlit-Application/chatlit.py
+++ 05-Launch-a-Chatlit-Application/chatlit.py
@@ -17,19 +17,18 @@ openai.api_type = "azure"
 openai.api_version = "2023-11-01"
 chat_deployment = os.getenv("AZURE_OPENAI_CHAT_DEPLOYMENT")
  
- 
 if "openai_model" not in st.session_state:
-   st.session_state["openai_model"] = chat_deployment 
+   #Hint: This line of code is used in st.session_state Streamlit applications for maintaining session states.
  
 if "messages" not in st.session_state:
-   st.session_state.messages = []
+   #Hint: This line of code is initializing st.session_state.messages to an empty list
  
 for message in st.session_state.messages:
    with st.chat_message(message["role"]):
        st.markdown(message["content"])
  
 if prompt := st.chat_input("What is up?"):
-   st.session_state.messages.append({"role": "user", "content": prompt})
+   #Hint: This line of code appends a new message to the 'messages' list in the session state. A message is a dictionary with two keys: 'role' and 'content'
    with st.chat_message("user"):
        st.markdown(prompt)
  
@@ -39,8 +38,7 @@ if prompt := st.chat_input("What is up?"):
        for response in openai.ChatCompletion.create(
            deployment_id=st.session_state["openai_model"],
            messages=[
-              {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
+              #Hint: This is a list comprehension in Python that generates a new list of dictionaries from st.session_state.messages. # Each dictionary in the new list has two keys: role and content.
            ],
            stream=True,
        ):
